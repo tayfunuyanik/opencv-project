c69cc03c547a17f2f00bed425407139d18834aeb
tayfunuyanik <tayfun@uyanik.dev>
2026-05-08 00:41:33 +0300

    Add color detection project

---
+++ 01-color-detection/color_detection.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lower_blue = np.array([100, 150, 50])
+    upper_blue = np.array([140, 255, 255])
+
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+    result = cv2.bitwise_and(frame, frame, mask=mask)
+
+    cv2.imshow("Kamera", frame)
+    cv2.imshow("Mavi Tespit", result)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
